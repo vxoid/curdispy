d0525f90d0ce01d653d4650b9bf70dd24638dc6c
idk <fanassasin777@ukr.net>
2023-03-27 20:12:57 +0300

    Http branch init with todo tasks

---
+++ curdis.py
@@ -0,0 +1,10 @@
+"""
+    TODO:
+    implement __init__ function that will authenticate user and get his app id
+    implement Channel, Message, Guild classes and their API (get, delete, create)
+    implement WebSocket events like MESSAGE_CREATE, MESSAGE_DELETE, INTERACTION_CREATE
+"""
+
+class Bot:
+    def __init__(self, token: str):
+        pass

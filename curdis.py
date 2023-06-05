30412d3d9b631e44862e2d5c7858bb5b0f6f1390
Denis <117019627+DENIS76T5@users.noreply.github.com>
2023-06-05 05:51:40 -0700

    append function send and delete message

--- curdis.py
+++ curdis.py
@@ -1,27 +1,38 @@
 import requests
 import json
-"""m
+"""
     TODO:
     implement init function that will authenticate user and get his app id
     implement Channel, Message, Guild classes and their _api (get, delete, create)
     implement WebSocket events like MESSAGE_CREATE, MESSAGE_DELETE, INTERACTION_CREATE
 """
-
+api = "https://discord.com/api/v9"
 class DiscordAPI:
     def __init__(self, token):
-        self._token = token
-        self.api = "https://discord.com/api/v9"
+        self.__token = token
         self._headers = {
-            "Authorization": f"Bot {self._token}"
+            "Authorization": f"Bot {self.__token}"
         }
-        response = requests.get(f"{self.api}/gateway/bot", headers=self._headers)
-        self.dick_url = json.loads(response.text)
-        print(f"url: {self.dick_url['url']}")
+        response = requests.get(f"{api}/gateway/bot", headers=self._headers)
+        response.raise_for_status()
+        self.api_url = json.loads(response.text)
         self.app_id = self._getAppId()
     def _getAppId(self):
-        response = requests.get(f"{self.api}/oauth2/applications/@me" , headers=self._headers)
+        response = requests.get(f"{api}/oauth2/applications/@me" , headers=self._headers)
         response.raise_for_status()
         app_data = json.loads(response.text)
-        return f"App ID: {app_data['id']}"
+        return app_data['id']
+    def send_message(self, message, channel_id):
+        payload = {
+            'content': message
+        }
+        response = requests.post(f"{api}/channels/{channel_id}/messages", headers=self._headers, json=payload)
+        response.raise_for_status()
+    def delete_mesage(self, message_id, channel_id):
+        response = requests.delete(f"{api}/channels/{channel_id}/messages/{message_id}", headers=self._headers)
+        response.raise_for_status()
+        
+
+
         
    
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from curdis import DiscordAPI
+import curdis
 TOKEN = "discord token"
-bot = DiscordAPI(token=TOKEN)
-print(bot.app_id)
+bot = curdis.DiscordAPI(token=TOKEN)
+# bot.send_message(message="xui", channel_id="1086953979948314694")
+# bot.delete_mesage(message_id="", channel_id="1086953979948314694")
+
 
 

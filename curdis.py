49058ad6198b54dc1af541caa67a797bb94442f7
Denis <117019627+DENIS76T5@users.noreply.github.com>
2023-06-23 05:03:18 -0700

    implement imba funcction

--- curdis.py
+++ curdis.py
@@ -1,38 +1,106 @@
-import requests
-import json
-"""
-    TODO:
-    implement init function that will authenticate user and get his app id
-    implement Channel, Message, Guild classes and their _api (get, delete, create)
-    implement WebSocket events like MESSAGE_CREATE, MESSAGE_DELETE, INTERACTION_CREATE
-"""
+import requests, json
+
 api = "https://discord.com/api/v9"
 class DiscordAPI:
     def __init__(self, token):
         self.__token = token
-        self._headers = {
-            "Authorization": f"Bot {self.__token}"
+        self.__headers = {
+            "Authorization": f"Bot {self.__token}",
+            "Content-Type": "application/json"
         }
-        response = requests.get(f"{api}/gateway/bot", headers=self._headers)
+        response = requests.get(f"{api}/gateway/bot", headers=self.__headers)
         response.raise_for_status()
         self.api_url = json.loads(response.text)
         self.app_id = self._getAppId()
+
     def _getAppId(self):
-        response = requests.get(f"{api}/oauth2/applications/@me" , headers=self._headers)
+        response = requests.get(f"{api}/oauth2/applications/@me" , headers=self.__headers)
         response.raise_for_status()
         app_data = json.loads(response.text)
-        return app_data['id']
-    def send_message(self, message, channel_id):
-        payload = {
-            'content': message
+        return app_data["id"]
+    
+    def get_all_messages(self, channel_id):
+        responce = requests.get(f"{api}/channels/{channel_id}/messages", headers=self.__headers)
+        responce.raise_for_status
+        messages = json.loads(responce.text)
+        all_messages = []
+        for message in messages:
+            content = message["content"]
+            id = message["id"]
+            channel_id = message["channel_id"]
+            author = message["author"]
+            user_id = author["id"]
+            all_messages.append(Message(self.__token, content, channel_id, id, user_id))
+        return all_messages
+
+    def send_message(self, message=None, channel_id=None, files=None):
+        headers = {
+            "Authorization": f"Bot {self.__token}"
         }
-        response = requests.post(f"{api}/channels/{channel_id}/messages", headers=self._headers, json=payload)
+        payload = {
+            "content": message
+        }
+        data = {
+            "file": ("cum.mp4", files)
+        }
+        response = requests.post(f"{api}/channels/{channel_id}/messages", headers=headers, json=payload,)
         response.raise_for_status()
-    def delete_mesage(self, message_id, channel_id):
-        response = requests.delete(f"{api}/channels/{channel_id}/messages/{message_id}", headers=self._headers)
-        response.raise_for_status()
-        
 
 
-        
-   
+
+    def register_commands(self):
+        command_data = {
+         "name": "spam",
+         "description": "spartaaaa"
+        }
+        response = requests.post(f"{api}/applications/{self.app_id}/commands", headers=self.__headers, data=json.dumps(command_data))
+        response.raise_for_status()
+class Message:
+    def __init__(self, token, content, channel_id, message_id, user_id):
+        self.__token = token
+        self.__content = content
+        self.__channel_id = channel_id
+        self.__message_id = message_id
+        self.__user_id = user_id
+        self.__gulid = "1086953817184141322"
+        self.__headers = {
+            "Authorization": f"Bot {self.__token}",
+            "Content-Type": "application/json"
+        }
+    
+    def delete(self):
+        response = requests.delete(f"{api}/channels/{self.__channel_id}/messages/{self.__message_id}", headers=self.__headers)
+        response.raise_for_status()
+
+    def kick_member(self):
+        payload = {
+            'reason': 'violation'
+        }
+        response = requests.delete(f"{api}/guilds/{self.__gulid}/members/{self.__user_id}", headers=self.__headers, json=payload)
+        response.raise_for_status()
+
+    def send_message(self):
+        headers = {
+            "Authorization": f"Bot {self.__token}"
+        }
+        payload = {
+            "content": "забанив лошка 😈"
+        }
+        with open("equipment/cum.mp4", "rb") as file:
+            f = file.read()
+        data = {
+            "file": ("cum.mp4", f)
+        }
+        response = requests.post(f"{api}/channels/{self.__channel_id}/messages", json=payload, headers=headers)
+        response = requests.post(f"{api}/channels/{self.__channel_id}/messages", json=payload, headers=headers, files=data)
+
+        response.raise_for_status()
+
+    def get_usr_id(self):
+        return self.__user_id
+
+    def get_content(self):
+        return self.__content
+    
+
+
--- main.py
+++ main.py
@@ -1,8 +1,64 @@
-import curdis
-TOKEN = "discord token"
+import curdis, time,  asyncio, json
+TOKEN = "MTA5MDI0Mzk2Njc3Mjk4NTg5Nw.GTciW2.Z9_PLAPyDuRoMKZuN8iZSduFNtLPFXZcelydG4"
 bot = curdis.DiscordAPI(token=TOKEN)
-# bot.send_message(message="xui", channel_id="1086953979948314694")
-# bot.delete_mesage(message_id="", channel_id="1086953979948314694")
+channel_id = "1086953979948314694"
+
+def delete_all_messages():
+    messages = bot.get_all_messages(channel_id=channel_id)
+    print(len(messages))
+    i = 0
+    while i < len(messages):
+        print(f"deleting {messages[i].get_content()}")
+        messages[i].delete()
+        time.sleep(0.5)
+        print(len(messages))
+        i+=1
+
+def delete_curse():
+    with open('equipment/curse.json', 'r') as file:
+        curse = json.load(file)
+    while True:
+        time.sleep(0.5)
+        messages = bot.get_all_messages(channel_id=channel_id)
+        for message in messages:
+            m = message.get_content()
+            for c in curse:
+                if c in m.lower() and message.get_usr_id() != bot.app_id:
+                    message.delete()
+                    print('deleting message')
+                    time.sleep(0.4)
+                    try:
+                        if 'бан' in m.lower():
+                            print('lox was banned')
+                            message.kick_member()
+                            message.send_message()
+                    except Exception as a:
+                        print(f"failed banned\n{a}")
+                        bot.send_message("бачу маєш адмінку какіш😈", channel_id=channel_id)
+
+
+delete_curse()
+
+# curse = "fuck shit dick bitch pussy stupid fucking блять сука підор гандон ганвнюк чмо какіш бан нахуй "
+# c = curse.split()
+# with open('equipment/curse.json', 'w', encoding='utf-8') as file:
+    # json.dump(c, file, ensure_ascii=False)
+
+# with open("cum.mp4", "rb") as file:
+#     f = file.read()
+# bot.send_message(channel_id="1086953979948314694",  files=f)
+
+# b= bot.get_all_messages(channel_id=channel_id)
+# for m in b:
+#     m.send_message()
 
 
 
+# botik = ws.WSBot()
+# async def handle_message(id, content,):
+#     print(f"Received message: {content}")
+#     pass
+# command_handlers = {
+#     # "command_name": command_handler_function,
+# }
+# asyncio.run(botik.run(handle_message))
